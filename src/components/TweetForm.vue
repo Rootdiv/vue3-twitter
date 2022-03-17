2a716b36b07fabfaff3d2a14062d2adf51bd0b70
Rootdiv <rootdiv@mail.ru>
2022-03-17 20:11:55 +0300

    feat: ref and Composition API

--- src/components/TweetForm.vue
+++ src/components/TweetForm.vue
@@ -7,23 +7,43 @@
 </template>
 
 <script>
+  import { ref } from 'vue';
   export default {
-    data() {
-      return {
-        body: '',
-      };
-    },
-    methods: {
-      onSubmit() {
-        this.$emit('onSubmit', {
+    emits: ['onSubmit'],
+    //При деструктуризации context необходимо указать props или знак подчёркивания
+    setup(_, { emit }) {
+      const body = ref('');
+      const onSubmit = () => {
+        emit('onSubmit', {
           id: Math.round(Math.random() * 30),
           avatar: `https://avatars.dicebear.com/api/male/${Date.now()}.svg`,
-          body: this.body,
+          body: body.value,
           likes: 0,
-          date: new Date(Date.now()).toLocaleDateString(),
+          date: new Date(Date.now()).toLocaleString(),
         });
-        this.body = '';
-      },
+        body.value = '';
+      };
+      return {
+        body,
+        onSubmit,
+      };
     },
+    // data() {
+    //   return {
+    //     body: '',
+    //   };
+    // },
+    // methods: {
+    //   onSubmit() {
+    //     this.$emit('onSubmit', {
+    //       id: Math.round(Math.random() * 30),
+    //       avatar: `https://avatars.dicebear.com/api/male/${Date.now()}.svg`,
+    //       body: this.body,
+    //       likes: 0,
+    //       date: new Date(Date.now()).toLocaleString(),
+    //     });
+    //     this.body = '';
+    //   },
+    // },
   };
 </script>
--- src/components/TweetsList.vue
+++ src/components/TweetsList.vue
@@ -1,10 +1,15 @@
 <template>
+  <select v-model="sortBy">
+    <option value="date">Сортировать по дате</option>
+    <option value="likes">Сортировать по лайкам</option>
+  </select>
   <ul>
-    <TweetItem v-for="item in items" :key="item.id" :item="item" />
+    <TweetItem v-for="item in sortedItems" :key="item.id" :item="item" />
   </ul>
 </template>
 
 <script>
+  import { ref, toRefs, computed } from 'vue';
   import TweetItem from '@/components/TweetItem';
   export default {
     components: { TweetItem },
@@ -13,6 +18,29 @@
         type: Array,
         required: true,
       },
+    },
+    setup(props) {
+      const { items } = toRefs(props);
+
+      const sortBy = ref('date');
+      const sortedItems = computed(() => {
+        const copy = items.value;
+
+        copy.sort((a, b) => {
+          if (a[sortBy.value] > b[sortBy.value]) {
+            return -1;
+          }
+          if (a[sortBy.value] < b[sortBy.value]) {
+            return 1;
+          }
+        });
+        return copy;
+      });
+
+      return {
+        sortBy,
+        sortedItems,
+      };
     },
   };
 </script>
